c18db65ee4cddc29ff0ab7ce7e600249bebedfdb
Kevin Zhang <kzhaaang@gmail.com>
2015-05-09 15:50:50 -0400

    Add configuration file

---
+++ config.js
@@ -0,0 +1,9 @@
+/* BEGIN CONFIGURATION FILE
+ * Anywhere configuration options are needed, just require() this script.
+ */
+
+var config = {}
+
+config.motd = "Message of the day!"; // message of the day; shows up in the top of the chat
+
+module.exports = config;
