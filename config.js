9e075f35590f96647647ed08fd9ec03f77d5d141
tenks <tenkser@googlemail.com>
2015-05-11 12:49:35 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,5 +7,6 @@ var config = {}
 config.motd = "Message of the day!"; // message of the day; shows up in the top of the chat
 config.autoaway_time = 0.1 //amount of time a user has to idle to trigger auto away (in minutes)
 config.typing_time = 2000; // amount of time chat displays user as typing after last keypress
+config.playback = 10; //number of messages that are played back on joining the chat
 
 module.exports = config;
