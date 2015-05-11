3cd49550be56f03fab757c3683931823bd8c36ee
tenks <tenkser@googlemail.com>
2015-05-11 12:26:37 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 var server = require('./server.js');
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var request = require('request');
 var app = express();
 
 app.io = server();
